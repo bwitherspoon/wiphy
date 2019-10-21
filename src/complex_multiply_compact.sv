0e390875aac58b0439b16c41a2d7f9c654e9ba3c
Brett Witherspoon <spoonb@cdspooner.com>
2019-10-21 10:39:17 -0500

    complex_multiply_compact: add compact complex multiplier
    This complex multiplier uses 3 real multipliers instead of 4, but
    has greater latency and probably worse timing.

---
+++ src/complex_multiply_compact.sv
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2019 Brett Witherspoon
+ */
+
+module complex_multiply_compact #(
+  parameter int WIDTH = 16,
+  parameter bit RESET = 0
+)(
+  input  logic               clk,
+  input  logic               reset,
+
+  input  logic               s_valid,
+  output logic               s_ready,
+  input  logic [4*WIDTH-1:0] s_data,
+
+  output logic               m_valid,
+  input  logic               m_ready,
+  output logic [4*WIDTH-1:0] m_data
+);
+  localparam int DELAY = 6;
+
+  typedef logic signed [WIDTH-1:0] data_t;
+  typedef logic signed [WIDTH:0] wide_t;
+  typedef logic signed [2*WIDTH-1:0] mult_t;
+  typedef logic signed [2*WIDTH:0] accu_t;
+
+  data_t a_real [4];
+  data_t a_imag [4];
+
+  data_t b_real [3];
+  data_t b_imag [3];
+
+  wide_t a_real_minus_a_imag;
+
+  accu_t a_real_minus_a_imag_x_b_imag;
+
+  accu_t common [3];
+
+  wide_t b_real_minus_b_imag;
+
+  accu_t b_real_minus_b_imag_x_a_real;
+
+  wide_t b_real_plus_b_imag;
+
+  accu_t b_real_plus_b_imag_x_a_imag;
+
+  accu_t p_real;
+  accu_t p_imag;
+
+  logic [DELAY-1:0] valid = '0;
+
+  always_ff @(posedge clk) begin
+    if (s_valid && s_ready) begin
+      {b_imag[0], b_real[0], a_imag[0], a_real[0]} <= s_data;
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    a_real[1] <= a_real[0];
+    a_imag[1] <= a_imag[0];
+    a_real[2] <= a_real[1];
+    a_imag[2] <= a_imag[1];
+    a_real[3] <= a_real[2];
+    a_imag[3] <= a_imag[2];
+    b_real[1] <= b_real[0];
+    b_real[2] <= b_real[1];
+    b_imag[1] <= b_imag[0];
+    b_imag[2] <= b_imag[1];
+  end
+
+  always_ff @(posedge clk) begin
+    a_real_minus_a_imag <= a_real[0] - a_imag[0];
+    a_real_minus_a_imag_x_b_imag <= a_real_minus_a_imag * b_imag[1];
+    common[0] <= a_real_minus_a_imag_x_b_imag;
+    common[1] <= common[0];
+    common[2] <= common[0];
+  end
+
+  always_ff @(posedge clk) begin
+    b_real_minus_b_imag <= b_real[2] - b_imag[2];
+    b_real_minus_b_imag_x_a_real <= b_real_minus_b_imag * a_real[3];
+  end
+
+  always_ff @(posedge clk) begin
+    b_real_plus_b_imag <= b_real[2] + b_imag[2];
+    b_real_plus_b_imag_x_a_imag <= b_real_plus_b_imag * a_imag[3];
+  end
+
+  always_comb begin
+    p_real = b_real_minus_b_imag_x_a_real + common[1];
+    p_imag = b_real_plus_b_imag_x_a_imag + common[2];
+  end
+
+  always_ff @(posedge clk) begin
+    if (!m_valid || m_ready) begin
+      m_data <= {p_imag[2*WIDTH:1], p_real[2*WIDTH:1]};
+    end
+  end
+
+  always_ff @(posedge clk) begin
+    if (RESET && reset) begin
+      valid <= '0;
+    end else if (!m_valid || m_ready) begin
+      valid <= {valid[$bits(valid)-2:0], s_valid};
+    end
+  end
+
+  assign m_valid = valid[$bits(valid)-1];
+
+  assign s_ready = m_ready;
+
+  wire unused = &{1'b0, p_real[0], p_imag[0]};
+
+endmodule
---
+++ src/tb/complex_multiply_compact_tb.sv
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 Brett Witherspoon
+ */
+
+module complex_multiply_compact_tb;
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  localparam int N = 256;
+
+  logic clk = 0;
+  always #5ns clk = ~clk;
+
+  logic reset = 1;
+  initial begin
+    repeat (2) @(posedge clk);
+    @(negedge clk) reset = 0;
+  end
+
+  logic s_valid = 0;
+  logic s_ready;
+  logic [1:0][31:0] s_data;
+
+  logic m_valid;
+  logic m_ready = 0;
+  logic [63:0] m_data;
+
+  complex_multiply_compact #(.WIDTH(16)) dut (.*);
+
+  initial begin
+    wait (!reset) @(negedge clk);
+
+    for (int i = 0; i < N; i++) begin
+      s_valid = 1;
+      s_data = {i[15:0], 16'd0, 16'd0, i[15:0]};
+      do @(posedge clk); while (s_ready !== 1);
+      #1 s_valid = 0;
+    end
+  end
+
+  initial begin
+    $dumpfile("complex_multiply_compact.fst");
+    $dumpvars(1, dut);
+
+    wait (!reset) @(negedge clk);
+
+    m_ready = 1;
+    for (int i = 0; i < N; i++) begin
+      do @(posedge clk); while (m_valid !== 1);
+      assert($signed(m_data[63:32]) === i * i / 2) else begin
+        $fatal(0, "%0d != %0d", $signed(m_data[63:32]), i * i / 2);
+      end
+      assert($signed(m_data[31:0]) === 0) else begin
+        $fatal(0, "%0d != %0d", $signed(m_data[31:0]), 0);
+      end
+    end
+
+    $finish;
+  end
+
+endmodule
